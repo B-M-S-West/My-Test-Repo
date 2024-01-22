ab9219ee62f02ac0e3360022cdef44fe36c1d94e
Ben West <144233589+B-M-S-West@users.noreply.github.com>
2024-01-22 22:02:49 +0000

    Update test_file.py
    Update to add comments

--- test_file.py
+++ test_file.py
@@ -1,5 +1,9 @@
 # file to create code on and test
+# users first name
 first_name = "Ben"
+# users last name
 last_name = "West"
+# users age
 age = 32
+# statement to print user information
 print(first_name + " " + last_name)
